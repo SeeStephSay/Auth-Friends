0cb3de37e1bfe8c6c9fce3ac1a0eba84711cc01e
Stephanie Butenhof <1divinedevotion@gmail.com>
2020-02-16 23:01:56 -0600

    Added Friend

---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import {
+  Button,
+  Row,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input
+} from "reactstrap";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import Friend from './Friend';
+
+const FriendList = props => {
+  
+}
+
+export default FriendList;
